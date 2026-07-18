6f25565a350e20bf6d44e7717608e997b68efb0a
Prototean <teanchai_ch@cmu.ac.th>
2026-07-18 16:10:03 +0700

    Update link to event id

--- src/components/EventDetailView.vue
+++ src/components/EventDetailView.vue
@@ -3,10 +3,15 @@ import { ref, onMounted } from 'vue'
 import { type Event } from '@/types'
 import EventService from '@/services/EventService'
 const event = ref<Event | null>(null)
-const id = ref<number>(5928101)
+const props = defineProps({
+    id: {
+        type: String,
+        required: true
+    }
+})
 onMounted(() => {
     // fetch event (by id) and set local event data
-    EventService.getEvent(id.value)
+    EventService.getEvent(Number(props.id))
         .then((response) =>{
             event.value = response.data
         })
--- src/router/index.ts
+++ src/router/index.ts
@@ -13,9 +13,10 @@ const router = createRouter({
       component: EvenListView,
     },
     {
-      path:'/event/5928101',
+      path:'/event/:id',
       name:'event-detail-view',
-      component: EventDetailView
+      component: EventDetailView,
+      props: true
     },
     {
       path: '/about',
