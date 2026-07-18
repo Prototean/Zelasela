e6f8416d1ffa49b8f53efc339aecb27298a50be3
Prototean <teanchai_ch@cmu.ac.th>
2026-07-18 14:53:58 +0700

    Update DetailViewEvent

---
+++ src/components/EventDetailView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { type Event } from '@/types'
+import EventService from '@/services/EventService'
+const event = ref<Event | null>(null)
+const id = ref<number>(5928101)
+onMounted(() => {
+    // fetch event (by id) and set local event data
+    EventService.getEvent(id.value)
+        .then((response) =>{
+            event.value = response.data
+        })
+        .catch((error) => {
+            console.error('There was an error!', error)
+        })
+})
+</script>
+<template>
+    <div v-if="event">
+        <h1>{{ event.title }} on {{ event.date }} @ {{  event.location }}</h1>
+        <p>{{ event.description }}</p>
+    </div>
+</template>
