7686b2a01adb9229a33065858d2057ed71fd0018
Prototean <teanchai_ch@cmu.ac.th>
2026-07-20 15:20:02 +0700

    Add button to registration page

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
 const props = defineProps<{
@@ -6,7 +7,14 @@ const props = defineProps<{
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+const router = useRouter()
+const register = () => {
+    // If the registration API call successful
+    // Push back to the event details view
+    router.push({ name: 'event-detail-view'})
+}
 </script>
 <template>
     <p>Register event here</p>
+    <button @clikc="register">Register</button>
 </template>
